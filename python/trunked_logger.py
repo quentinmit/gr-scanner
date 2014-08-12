91d8aa73b077be54272e5b0b5b72ba92d08267b5
Quentin Smith <quentin@mit.edu>
2014-08-12 17:42:18 -0400

    Implement more smartnet messages

--- python/trunked_logger.py
+++ python/trunked_logger.py
@@ -7,17 +7,83 @@ import osmosdr
 from optparse import OptionParser, OptionGroup
 import logging
 
+class SmartnetCmd(object):
+    OSW_UNK1 = 0x0273
+    OSW_UNK2 = 0x029B
+    OSW_UNK3 = 0x02a9
+    OSW_UNK4 = 0x02c3
+    OSW_UNK5 = 0x0259
+    OSW_UNK6 = 0x0279
+    OSW_UNK7 = 0x02af
+    OSW_BACKGROUND_IDLE = 0x02f8
+    OSW_FIRST_CODED_PC = 0x0304
+    OSW_FIRST_NORMAL = 0x0308
+    OSW_FIRST_TY2AS1 = 0x0309
+    OSW_EXTENDED_FCN = 0x030b
+    OSW_AFFIL_FCN = 0x030d
+    OSW_TY2_AFFILIATION = 0x0310
+    OSW_TY1_STATUS_MIN = 0x0310
+    OSW_TY2_MESSAGE = 0x0311
+    OSW_TY1_STATUS_MAX = 0x0317
+    OSW_TY1_ALERT = 0x0318
+    OSW_TY1_EMERGENCY = 0x0319
+    OSW_TY2_CALL_ALERT = 0x0319
+    OSW_SYSTEM_UNK1 = 0x0320
+    OSW_FIRST_ASTRO = 0x0321
+    OSW_SYSTEM_CLOCK = 0x0322
+    OSW_SCAN_MARKER = 0x032b
+    OSW_EMERG_ANNC = 0x032e
+    OSW_AMSS_ID_MIN = 0x0360
+    OSW_AMSS_ID_MAX = 0x039f
+    OSW_CW_ID = 0x03a0
+    OSW_SYS_NETSTAT = 0x03bf
+    OSW_SYS_STATUS = 0x03c0
+
 class smartnet_message(object):
-    def __init__(self, msg, cmd_to_freq = None):
+    # Known commands:
+    # < 0x2d0 = frequency
+    # 0x308 = continuation, addr is "target mobile ID code" (fig 12 in patent)
+    def __init__(self, msg, linked, cmd_to_freq = None):
         self.string = msg.to_string()
+        self.linked = list(linked)
         [self.addr, self.groupflag, self.cmd] = [int(i) for i in self.string.split(',')]
         self.tg = self.addr & 0xFFF0
         self.prio = self.addr & 0xF
         self.freq = cmd_to_freq and cmd_to_freq(None, self.cmd)
+
+        if self.cmd == SmartnetCmd.OSW_SYS_STATUS:
+            value = self.addr << 1 | self.groupflag
+            self.group_timeout = value & 0x1f
+            value >>= 5
+            self.connect_timeout = value & 0x1f
+            value >>= 5
+            self.dispatch_timeout = value & 0xf
+            value >>= 4
+            self.power = value & 0x1
+            value >>= 1
+            self.opcode = value
+        elif self.cmd == SmartnetCmd.OSW_SYS_NETSTAT:
+            pass
+        elif self.cmd == SmartnetCmd.OSW_EXTENDED_FCN:
+            pass
     def __str__(self):
         if self.freq:
             return "TALK: Group %04x (Prio %1x) Cmd %03x Frequency %f" % (
                 self.tg, self.prio, self.cmd, self.freq)
+        elif self.cmd == SmartnetCmd.OSW_SYS_STATUS:
+            return "SYS_STATUS: Opcode %1x Power %d Timeouts: Dispatch %ds Connect %ds Group %ds" %(
+                self.opcode, self.power,
+                self.dispatch_timeout*30,
+                self.connect_timeout*30,
+                self.group_timeout*30)
+        elif self.cmd == SmartnetCmd.OSW_EXTENDED_FCN:
+            if (self.addr & 0xfc00) == 0x6000:
+                # SmartZone Peer info
+                if not self.linked:
+                    return "PEER_INFO: MISSING PREFIX PACKET Data %x" % (self.addr & 0x3ff)
+                return "PEER_INFO: SysID %04x Data %x" % (self.linked[-1].addr, self.addr & 0x3ff)
+            else:
+                return "EXTENDED_FCN: %s Addr %04x" % ("Group" if self.groupflag else "Radio", self.addr,)
         else:
             if self.groupflag:
                 return "CMD: Radio %04x Cmd %03x" % (self.addr, self.cmd)
@@ -30,13 +96,17 @@ class smartnet_all_rx(scanner.smartnet_ctrl_rx):
 
         self.linked_cmd = []
     def msg_handler(self, msg):
-        msg = smartnet_message(msg, self.cmd_to_freq)
+        msg = smartnet_message(msg, self.linked_cmd, self.cmd_to_freq)
         if msg.cmd == 0x308:
             # Continuation
             self.linked_cmd.append(msg)
         else:
             self.linked_cmd.append(msg)
-            logging.info(", ".join(str(msg) for msg in self.linked_cmd))
+            if msg.cmd not in (SmartnetCmd.OSW_SYS_STATUS,):
+                try:
+                    logging.info(", ".join(str(msg) for msg in self.linked_cmd))
+                except:
+                    logging.exception("Unable to log message")
             self.linked_cmd = []
 
 #instantiates a sample feed, control decoder, audio sink, and control
